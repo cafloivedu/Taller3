76e7e338f69a66cd877814ce95d479308958ebaf
cafloivedu <cafloivedu@outlook.com>
2020-02-18 20:16:47 -0500

    update

--- app/src/main/java/com/kafloid/taller3/UserAdapter.kt
+++ app/src/main/java/com/kafloid/taller3/UserAdapter.kt
@@ -5,8 +5,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.kafloid.taller3.data.User
+import com.kafloid.taller3.databinding.RowBinding
 import kotlinx.android.synthetic.main.row.view.*
 
 class UserAdapter(
@@ -17,32 +19,35 @@ class UserAdapter(
         parent: ViewGroup,
         viewType: Int
     ): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.row, parent, false)
-        return ViewHolder(view)
+        //val view = LayoutInflater.from(parent.context).inflate(R.layout.row, parent, false)
+        //return ViewHolder(view)
+        var binder : RowBinding
+        binder = DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.row, parent, false)
+        return ViewHolder(binder)
     }
 
     override fun getItemCount(): Int = mValues.size
 
     override fun onBindViewHolder(holder: UserAdapter.ViewHolder, position: Int) {
         val item = mValues[position]
-        holder.textView.text = item.nombre
+        holder.mView.user = item
+        holder.mView.executePendingBindings()
 
-        holder.mView.setOnClickListener {
+        /*holder.mView.setOnClickListener {
             mListener?.onListItemInteraction(item)
         }
 
         holder.button.setOnClickListener{
             mListener?.onListButtonInteraction(item)
-        }
+        }*/
     }
 
     public fun updateData(){
         notifyDataSetChanged()
     }
 
-    inner class ViewHolder(val mView: View) : RecyclerView.ViewHolder(mView){
-        val button : Button = mView.buttonDeleteUser
-        val textView : TextView = mView.textViewUserName
+    inner class ViewHolder(val mView: RowBinding) : RecyclerView.ViewHolder(mView.root){
+
     }
 
     interface onListInteraction {
