59544bcfb11f91eb3b8fd2ac42a7b78176a77b29
cafloivedu <cafloivedu@outlook.com>
2020-02-18 19:30:34 -0500

    update

--- app/src/main/java/com/kafloid/taller3/MainFragment.kt
+++ app/src/main/java/com/kafloid/taller3/MainFragment.kt
@@ -6,11 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kafloid.taller3.data.User
+import kotlinx.android.synthetic.main.fragment_main.view.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class MainFragment : Fragment() {
+
+    val users = mutableListOf<User>()
+    private var adapter : MyUserRecyclerViewAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +25,14 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_main, container, false)
+
+        users.add(User("User 1"))
+        users.add(User("User 2"))
+
+        adapter = MyUserRecyclerViewAdapter(users)
+
+        view.list.layoutManager = LinearLayoutManager(context)
+        view.list.adapter = adapter
 
         return view
     }
--- app/src/main/java/com/kafloid/taller3/MyUserRecyclerViewAdapter.kt
+++ app/src/main/java/com/kafloid/taller3/MyUserRecyclerViewAdapter.kt
@@ -1,8 +1,34 @@
 package com.kafloid.taller3
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.kafloid.taller3.data.User
+import kotlinx.android.synthetic.main.row.view.*
 
 class MyUserRecyclerViewAdapter(
-    private val mValues: List<User>) : RecyclerView.Adapter<MyUserRecyclerViewAdapter.ViewHolder> {
+    private val mValues: List<User>) : RecyclerView.Adapter<MyUserRecyclerViewAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.row, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = mValues.size
+
+    override fun onBindViewHolder(holder: MyUserRecyclerViewAdapter.ViewHolder, position: Int) {
+        val item = mValues[position]
+        holder.textView.text = item.nombre
+    }
+
+    inner class ViewHolder(val mView: View) : RecyclerView.ViewHolder(mView){
+        val button : Button = mView.buttonDeleteUser
+        val textView : TextView = mView.textViewUserName
+    }
+
 }
